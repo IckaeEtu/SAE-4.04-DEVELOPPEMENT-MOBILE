82741d0c3c7b2040156f7a8c5e83c1c81ea848b5
IckaeEtu <mickael.riego@etu.univ-orleans.fr>
2025-03-24 15:24:56 +0100

    ajout du routeur

---
+++ lib/routes/router.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:go_routeur/go_routeur.dart';
+
+import 'RestaurantDetailScreen.dart';
+import 'HomePage.dart';
+
+// Pour l'utiliser il faut mettre dans le bouton "contect.go('lien de la page: /restaurant/123)"
+final GoRouteur routeur = GoRouteur(
+    routes: <RouteBase>[
+        GoRoute(
+            path: '/',
+            builder: () {
+                return HomePage();
+            },
+        ),
+        GoRoute(
+            path: '/restaurant/:id',
+            builder: (context, state) {
+                final id = int.parse(state.params['id']);
+                return RestaurantDetailScreen(restaurantId: id);
+            },
+        ),
+        // Les autres routes qu'on devra mettre ..
+    ]
+)
